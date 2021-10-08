2344dbce0244b5c39eb79224b38eccfffffe8b51
pawanrj <chavanpawan128@gmail.com>
2021-10-08 14:38:43 +0530

    IES-2:In HomeController class Show method Added

--- src/main/java/com/javaboys/controller/HomeController.java
+++ src/main/java/com/javaboys/controller/HomeController.java
@@ -1,5 +1,18 @@
 package com.javaboys.controller;
 
+import org.springframework.stereotype.Controller;
+
+import com.javaboys.service.EmployeeServiceI;
+
+@Controller
 public class HomeController {
+	
+	private EmployeeServiceI employeeServiceI;
+	
+	public void showmsg () {
+		
+		System.out.println(" Hello Vikas Sir");
+	}
+	
 
 }
